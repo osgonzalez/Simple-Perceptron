0b7732282eb8da0e4c9e7208603fa505c26711d5
osgonzalez <osgonzalez@esei.uvigo.es>
2019-03-18 12:22:58 +0100

    Add training method

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -5,7 +5,8 @@ public class Perceptron {
 	private double[] pesos;
 	private int numeroInputs;
 	private static final double TASA_APRENDIZAJE = 0.5d;
-	
+	private static final String NUMERO_INPUTS_INCORRECTO = "Error!, El numero de Inputs "
+			+ "proporcionado no se corresponde con el numero de Inputs del perceptron";
 	
 	
 	
@@ -45,7 +46,54 @@ public class Perceptron {
 	}
 
 
+	public int calcularOutput(double[] inputs) throws Exception {
+		if(inputs.length != this.numeroInputs) {
+			throw new Exception(NUMERO_INPUTS_INCORRECTO);
+		}
+		
+		double suma = 0;
+		
+		for(int i=0; i<this.numeroInputs; i++) {
+			suma += inputs[i] * this.pesos[i];
+		}
+		
+		return suma >= 0 ? 1 : -1;
+	}
 	
+	public boolean entrenar(double[][] listaInputs, int[] listaResultadosEsperados , int numIteracionesMaximas) throws Exception {
+		boolean entrenamientoCorrectamenteFinalizado = false;
+		int numIteracionActual = 0;
+		while(!entrenamientoCorrectamenteFinalizado && numIteracionActual < numIteracionesMaximas) {
+			entrenamientoCorrectamenteFinalizado = true;
+			
+			for(int i=0; i < listaInputs.length; i++) {
+				
+				int outputObtenido = calcularOutput(listaInputs[i]);
+				int outputEsperado = listaResultadosEsperados[i];
+				
+				String log = listaInputs[i] + " => " + "outputEsperado: " + outputEsperado
+						+ " ,outputObtenido: " + outputObtenido;
+				
+				if(outputObtenido != outputEsperado) {
+					entrenamientoCorrectamenteFinalizado = false;
+					recalcular(listaInputs[i], outputObtenido, outputEsperado);
+					log += " (Error)";
+					break;
+				}
+				
+				System.out.println(log);
+			}
+				
+			
+			
+		}
+		
+		return entrenamientoCorrectamenteFinalizado;
+	}
 	
 
+	public void recalcular(double[] inputs, int outputObtenido, int outputEsperado) {
+		
+	}
+	
 }
